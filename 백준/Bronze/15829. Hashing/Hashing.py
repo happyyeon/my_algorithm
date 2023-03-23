3f20045fcec10a158fd684bd80b1b442cfb1e3a5
Jeongyeon Lee <88064555+happyyeon@users.noreply.github.com>
2023-03-23 09:31:22 +0900

    Update Hashing.py

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,11 +1,12 @@
+INF = 1234567891
 def convert(string):
     number = 0
     alpha_to_num = dict()
     for i in range(97,124):
         alpha_to_num[chr(i)] = i-96
     for i in range(len(string)):
-        number += (alpha_to_num[string[i]]*(31**i)) % 1234567891
-    return number
+        number += (alpha_to_num[string[i]]*(31**i)) % INF
+    return number % INF
 
 if __name__ == '__main__':
     L = int(input())
